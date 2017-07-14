9ca58325a6748faa7d3360168b9e01f6d5c4d680
Max Howell <mxcl@me.com>
2017-07-14 16:02:27 -0400

    Fix race

--- Sources/race.swift
+++ Sources/race.swift
@@ -1,9 +1,5 @@
 @inline(__always)
-public func race<U: Thenable>(_ thenables: U...) -> Promise<U.T> {
-    return race(thenables)
-}
-
-public func race<U: Thenable>(_ thenables: [U]) -> Promise<U.T> {
+private func _race<U: Thenable>(_ thenables: [U]) -> Promise<U.T> {
     let result = Promise<U.T>(.pending)
     for thenable in thenables {
         thenable.pipe{ result.schrödinger = .resolved($0) }
@@ -11,16 +7,21 @@ public func race<U: Thenable>(_ thenables: [U]) -> Promise<U.T> {
     return result
 }
 
-@inline(__always)
-public func race<T>(_ guarantees: Guarantee<T>...) -> Guarantee<T> {
-    return race(guarantees)
+public func race<U: Thenable>(_ thenables: U...) -> Promise<U.T> {
+    return _race(thenables)
 }
 
-public func race<T>(_ guarantees: [Guarantee<T>]) -> Guarantee<T> {
+public func race<U: Thenable>(_ thenables: [U]) -> Promise<U.T> {
+    guard !thenables.isEmpty else {
+        return Promise(error: PMKError.badInput)
+    }
+    return _race(thenables)
+}
+
+public func race<T>(_ guarantees: Guarantee<T>...) -> Guarantee<T> {
     let (result, seal) = Guarantee<T>.pending()
-    for thenable in guarantees {
-        thenable.pipe(to: seal)
+    for guarantee in guarantees {
+        guarantee.pipe(to: seal)
     }
     return result
 }
-
